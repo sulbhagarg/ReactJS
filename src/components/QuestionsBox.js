f3ffca496ad6cd9c63ab034c9c651d45b1bc4a13
sulbhagarg <sulbhagarg99@gmail.com>
2020-10-20 18:49:32 +0530

    add answer options in components

--- src/components/QuestionsBox.js
+++ src/components/QuestionsBox.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 const QuestionBox = ({question, options}) => {
+    const [answer, setAnswer] = useState(options);
     return(
         <div className="questionBox">
             <div  className="question">{question}</div>
+            {answer.map((text, index) => (
+                <button key={index} className="answerBtn" onClick={() => {
+                    setAnswer([text]);
+                }}>
+                    {text}
+                </button>
+            ))}
         </div>
     )
 }
