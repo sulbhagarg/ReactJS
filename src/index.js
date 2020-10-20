6bb1a0448e6a881735f11173469cdc45341c30e7
sulbhagarg <sulbhagarg99@gmail.com>
2020-10-20 18:38:05 +0530

    add questions to the page

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./assets/style.css";
+import quizService from "./quizService";
+
+class QuizBee extends Component {
+    state = {
+        questionBank: []
+    };
+    getQuestions = () => {
+        quizService().then(question => {
+            this.setState({
+                questionBank: question
+            });
+        });
+    };
+    componentDidMount() {
+        this.getQuestions();
+    }
+    render() {
+        return (
+            <div className="container">
+                <div className="title">QuizBee</div>
+                {this.state.questionBank.length > 0 && 
+                    this.state.questionBank.map(({question, answers, correct, questionId}) => (
+                        <h4>{question}</h4>
+                    )
+                )};
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<QuizBee />, document.getElementById("root"));
