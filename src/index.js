db207a69957f469e893576848670ae9f44fa9fde
sulbhagarg <sulbhagarg99@gmail.com>
2020-10-20 18:59:35 +0530

    add scoring funtionality

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import QuestionBox from "./components/QuestionsBox";
 
 class QuizBee extends Component {
     state = {
-        questionBank: []
+        questionBank: [],
+        score: 0,
+        responses: 0
     };
     getQuestions = () => {
         quizService().then(question => {
@@ -15,6 +17,16 @@ class QuizBee extends Component {
             });
         });
     };
+    computeAnswer = (answer, correctAnswer) => {
+        if(answer === correctAnswer) {
+            this.setState({
+                score: this.state.score+1
+            });
+        }
+        this.setState({
+            responses: this.state.responses < 5 ? this.state.responses+1 : 5
+        });
+    }
     componentDidMount() {
         this.getQuestions();
     }
@@ -22,11 +34,17 @@ class QuizBee extends Component {
         return (
             <div className="container">
                 <div className="title">QuizBee</div>
-                {this.state.questionBank.length > 0 && 
+                {this.state.questionBank.length > 0 && this.state.responses < 5 &&
                     this.state.questionBank.map(({question, answers, correct, questionId}) => (
-                        <QuestionBox question={question} options={answers} key={questionId}/>
+                        <QuestionBox 
+                            question={question} 
+                            options={answers} 
+                            key={questionId}
+                            selected={answer =>  this.computeAnswer(answer, correct)}
+                        />
                     )
                 )};
+                {this.state.responses === 5 ? (<h2>{this.state.score}</h2>) : null}
             </div>
         );
     }
